f1d13fe9fb196a18a5e2e782337163ef10240dc8
matmat009 <vashdestampede0009@gmail.com>
2025-03-10 23:35:31 +0800

    Fixed Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/todo_list_app/", // Add this line for GitHub Pages deployment
   plugins: [react()],
-})
+});
